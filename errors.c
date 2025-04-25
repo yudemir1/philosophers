4fb4856bde99a46246389d06ea86340a64f35293
yudemir1 <yusufdemir5484@gmail.com>
2025-04-25 03:41:11 +0300

    error mesajlarına newline eklendi.

--- errors.c
+++ errors.c
@@ -6,7 +6,7 @@
 /*   By: yudemir <yudemir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 02:46:15 by yudemir           #+#    #+#             */
-/*   Updated: 2025/04/25 03:29:30 by yudemir          ###   ########.fr       */
+/*   Updated: 2025/04/25 03:40:49 by yudemir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ void	ft_max_int_error(int *arr, char **split)
 {
 	free_split(split);
 	free(arr);
-	printf("Error occured while parsing arguments.");
+	printf("Error occured while parsing arguments.\n");
 	exit(1);
 }
 
 void	ft_error()
 {
-	printf("Error");
+	printf("Error\n");
 	exit(1);
 }
 
