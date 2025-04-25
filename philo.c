73df4e0c7fcacd828897c80bdbcf54304ed0340a
yudemir1 <yusufdemir5484@gmail.com>
2025-04-25 13:47:05 +0300

    kucuk düzeltmeler

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: yudemir <yudemir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 05:42:51 by yudemir           #+#    #+#             */
-/*   Updated: 2025/04/25 03:20:01 by yudemir          ###   ########.fr       */
+/*   Updated: 2025/04/25 13:46:43 by yudemir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,17 @@ int main(int ac, char **av)
 {
 	t_rules *rules;
 	
-	rules = NULL;
-	parse(ac, av, &rules);
-	print_arguments(rules);
-
+	if (ac >= 2)
+	{
+		rules = NULL;
+		parse(ac, av, &rules);
+		print_arguments(rules);
+	}
+	else 
+	{
+		printf("Error\n");
+		return (1);
+	}
 	free(rules);
+	return (0);
 }
